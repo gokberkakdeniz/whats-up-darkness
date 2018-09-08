ec4ae3c3def08e43bfa9b65b0cb788ef78c9770d
tncga <qokberk_akdeniz@hotmail.com>
2018-09-08 19:39:47 +0300

    rmove test code

--- assets/libs/notification.js
+++ assets/libs/notification.js
@@ -12,35 +12,7 @@ setTimeout(() => {
       title,
       options
     })
-
-/*
-  test on windows
-
-  a = () => new Notification(Math.floor((Math.random() * 10) + 1))
-  setTimeout(function() {a(); setTimeout(function() {a(); setTimeout(function() {a(); setTimeout(function() {a()}, 50)}, 50)}, 50)}, 50)
-*/
-
-//     if (typeof options === "undefined") {
-//       options = {}
-//       options.tag = "no-tag"
-//     } else if (!options.hasOwnProperty("tag")) {
-//       options.tag = "no-tag"
-//     }
-//     if (global.notifications.hasOwnProperty(options.tag)) {
-//       console.log(title)
-//       new_notification = () => {
-//         global.notifications[options.tag].close.bind(global.notifications[options.tag])
-//         global.notifications[options.tag] = new OldNotification(title, options)
-//       }
-//       setTimeout(new_notification, 1500)
-//     } else {
-//         global.notifications[options.tag] = new OldNotification(title, options)
-//     }
-//
-//     global.notifications[options.tag].onclick = function(title, options) {
-//       electron.getCurrentWindow().show()
-//     }
-
+    
     return new OldNotification(title, options)
   }
   global.Notification.prototype = OldNotification.prototype
