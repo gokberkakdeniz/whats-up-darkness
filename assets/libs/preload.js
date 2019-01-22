df3b76707bfda027d8bd81ed1fbe5c8885d464c7
tncga <qokberk_akdeniz@hotmail.com>
2019-01-22 03:14:55 +0300

    fix blank screen

--- assets/libs/preload.js
+++ assets/libs/preload.js
@@ -1,18 +1,18 @@
 const {ipcRenderer} = require('electron')
 const electron = require("electron").remote
-global.notifications= {}
+global.notifications = {}
 
 // setTimeout: fix for async APIs
 // electron 3.x/4.x
 // https://github.com/electron/electron/issues/13787#issuecomment-413211266
 setTimeout(() => {
   const OldNotification = global.Notification
-  global.Notification = function(title, options) {
+  global.Notification = function (title, options) {
     ipcRenderer.send('notification-triggered', {
       title,
       options
     })
-    
+
     return new OldNotification(title, options)
   }
   global.Notification.prototype = OldNotification.prototype
@@ -22,10 +22,10 @@ setTimeout(() => {
 
 // https://stackoverflow.com/a/53269990/8521693
 const checkElement = async selector => {
-  while ( document.querySelector(selector) === null) {
-      await new Promise( resolve =>  requestAnimationFrame(resolve) )
+  while (document.querySelector(selector) === null) {
+    await new Promise(resolve => requestAnimationFrame(resolve))
   }
-  return document.querySelector(selector) 
+  return document.querySelector(selector)
 }
 
 // Update chrome fix
@@ -34,24 +34,26 @@ const checkElement = async selector => {
 var ses = electron.session.defaultSession; //Gets the default session
 ses.flushStorageData(); //Writes any unwritten DOMStorage data to disk
 ses.clearStorageData({ //Clears the specified storages in the session
-    storages: ['appcache', 'serviceworkers', 'cachestorage', 'websql', 'indexdb'],
+  storages: ['appcache', 'serviceworkers', 'cachestorage', 'websql', 'indexdb'],
 })
 
 window.navigator.serviceWorker.getRegistrations().then(registrations => {
-    for (let registration of registrations) {
-        registration.unregister(); //Unregisters all the service workers
-    }
+  for (let registration of registrations) {
+    registration.unregister(); //Unregisters all the service workers
+  }
 })
 
-window.onload = function() {
-  const titleEl = document.querySelector('.window-title');
-  if (titleEl && titleEl.innerHTML.includes('Google Chrome 36+')) {
+document.onreadystatechange = function () {
+  if (document.readyState === "interactive" || document.readyState === "complete") {
+    const titleEl = document.querySelector('.window-title');
+    if (titleEl && titleEl.innerHTML.includes('Google Chrome 36+')) {
       window.location.reload(); //Reloads the page if the page shows the error
+    }
+    
+    //Watch 'Click to update WhatsApp' alert
+    checkElement(".m6ZEb").then(el => {
+      el.style.display = "none"
+      console.log("Update alert removed.")
+    })
   }
-
-  //Watch 'Click to update WhatsApp' alert
-  checkElement(".m6ZEb").then(el => {
-    el.style.display="none"
-    console.log("Update alert removed.")
-  })
 }
