dabf8d311334fcc97ce2f522d2b1b1dbf67b5cb2
tncga <qokberk_akdeniz@hotmail.com>
2019-02-11 00:29:27 +0300

    fix god level of logical stupidness

--- assets/libs/urlTool.js
+++ assets/libs/urlTool.js
@@ -8,8 +8,8 @@ class URL {
             "^(?:https|http):\/\/open\\.spotify\\.com\/track\/(?<track>\\w+)": [["track"], track => "spotify:track:" + track],
             "^(?:https|http):\/\/open\\.spotify\\.com\/artist\/(?<artist>\\w+)": [["artist"], artist => "spotify:artist:" + artist],
             "^(?:https|http):\/\/open\\.spotify\\.com\/album\/(?<album>\\w+)": [["album"], album => "spotify:album:" + album],
-            "^(?:https|http):\/\/open\\.spotify\\.com\/user\/(?<user>\\w+)(?:\\?|$)": [["user"], user => "spotify:user:" + user],
-            "^(?:https|http):\/\/open\\.spotify\\.com\/user\/(?<user>\\w+)\/playlist\/(?<playlist>\\w+)(?:\\?|$)": [["user", "playlist"], (user, playlist) => "spotify:user:" + user + ":playlist:" + playlist]
+            "^(?:https|http):\/\/open\\.spotify\\.com\/user\/(?<user>\\w+)": [["user"], user => "spotify:user:" + user],
+            "^(?:https|http):\/\/open\\.spotify\\.com\/user\/(?<user>\\w+)\/playlist\/(?<playlist>\\w+)": [["user", "playlist"], (user, playlist) => "spotify:user:" + user + ":playlist:" + playlist]
           }
     }
 
@@ -22,10 +22,9 @@ class URL {
             if(matches) {
                 const parameters = parameter_names.map(name => matches.groups[name])
                 return converter.bind.apply(converter, [null].concat(Array.prototype.slice.call(parameters)))()
-            } else {
-                return url
             }
         }
+        return url
     }
 }
 
